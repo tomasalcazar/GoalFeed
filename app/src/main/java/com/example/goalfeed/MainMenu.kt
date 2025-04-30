bca749c7557441fbd15e91dfcedfda1db8154c79
zartf <tomas.alcazar@ing.austral.edu.ar>
2025-04-30 17:16:34 -0300

    only layout & theme with *

--- app/src/main/java/com/example/goalfeed/MainMenu.kt
+++ app/src/main/java/com/example/goalfeed/MainMenu.kt
@@ -1,8 +1,13 @@
 package com.example.goalfeed
 
 import androidx.compose.foundation.layout.*
-import androidx.compose.material3.*
-import androidx.compose.runtime.*
+import androidx.compose.material3.Button
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.hilt.navigation.compose.hiltViewModel
--- app/src/main/java/com/example/goalfeed/home/NewsDetail.kt
+++ app/src/main/java/com/example/goalfeed/home/NewsDetail.kt
@@ -1,5 +1,9 @@
 import androidx.compose.foundation.layout.*
-import androidx.compose.material3.*
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SmallTopAppBar
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import coil3.compose.AsyncImage
